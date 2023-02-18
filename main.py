c9681e6d684cec3064a5b4de9cb52925b342f005
KylaLiwanag <kylacamilleliwanag@gmail.com>
2023-02-18 20:34:16 +0800

    RESTART

--- main.py
+++ main.py
@@ -22,6 +22,21 @@ top_left_y = s_height - play_height
 background = pygame.image.load("background.jpg")
 back = pygame.image.load('front_page.jpg')
 try_again = pygame.image.load('try_again.jpg ')
+
+#colors
+gray=(119,118,110)
+white=(255,255,255)
+black=(0,0,0)
+red=(200,0,0)
+green=(0,200,0)
+blue=(0,255,255)
+bright_red=(255,0,0)
+bright_green=(0,255,0)
+sea_green=(0, 255, 255)
+orange=(255, 165, 0)
+yellow=(255, 255, 0)
+purple=(128, 0, 128)
+light_red=(255, 0, 0)
 # SHAPE FORMATS
 
 S = [['.....',
@@ -158,12 +173,32 @@ def game_intro():
         s_height = 700
 
         message('TETRIS', red, 100, (s_width / 2 - 200), 100)
-        message("It Doesn't Stop Until You Die", white, 50, (s_width / 2 - 300), 200)
+        message("WELCOME TO  THE TETRIS WORLD!", white, 50, (s_width / 2 - 300), 200)
         # button(horizental location ,vertical location,right shift,left shift)
         button(100, 400, 70, 30, 'GO!', white, bright_red, red, 25, 106, 406, gameloop)
         button(600, 400, 70, 30, 'QUIT', white, bright_green, green, 25, 606, 406, quit1)
 
         pygame.display.update()
+
+
+def restart():
+    gameintro = False
+    while gameintro == False:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameintro = True
+                game_over = True
+
+        s_width = 900
+        s_height = 700
+
+        message("would you like to play again", yellow, 50, (s_width / 2 - 300), 200)
+        button(100, 400, 90, 30, 'play again', white, bright_red, red, 25, 106, 406, gameloop)
+        button(600, 400, 140, 30, 'No more TETRIS', white, bright_green, green, 25, 606, 406, quit1)
+
+        pygame.display.update()
+
 
 def create_grid(locked_positions={}):
     grid = [[(0, 0, 0) for x in range(10)] for x in range(20)]
