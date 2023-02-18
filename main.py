e3fd78e98610d2cd1099aafefe87ff3c04a13fcd
KylaLiwanag <kylacamilleliwanag@gmail.com>
2023-02-18 21:23:02 +0800

    score board

--- main.py
+++ main.py
@@ -378,6 +378,24 @@ def draw_next_shape(shape, surface):
     surface.blit(label, (sx + 10, sy - 30))
 
 
+def update_score(nscore):
+    score = max_score()
+
+    with open('scores.txt', 'w') as f:
+        if int(score) > nscore:
+            f.write(str(score))
+        else:
+            f.write(str(nscore))
+
+
+def max_score():
+    with open('scores.txt', 'r') as f:
+        lines = f.readlines()
+
+        score = lines[0].strip()
+
+    return score
+
 def draw_window(surface):
     surface.fill((0, 0, 0))
     # Tetris Title
